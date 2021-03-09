3711f96bf796487b91ab522748bb6c4de15f2d95
Nicholas Santos <nicholas.santos.stefanini@elo.com.br>
2021-03-08 21:38:52 -0300

    remove paths

--- src/Pages/Dashboard/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// import { Container } from './styles';
-
-function Dashboard() {
-  return <h1 style={{color:'#fff'}}>OlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOlaOla</h1>;
-}
-
-export default Dashboard;
--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,6 +1,5 @@
 import Home from './Home';
 import Login from './Login';
-import Music from './Music';
 import Album from './Album';
 
-export {Home,Music, Login,Album};
+export {Home,Login,Album};
